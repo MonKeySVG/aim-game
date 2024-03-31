d09f967faf1bb84d68ae816c588dcf9a48a11189
MonKeySVG <play95100@gmail.com>
2024-03-31 23:38:16 +0200

    Limitation du leaderboard a 5 entrées et blocage de tentative d'envoie de multiple fois le même score

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -21,6 +21,7 @@ export class ScoreboardComponent {
   score: number = 0;
   stats: any;
   leaderboard: any[] = [];
+  public scoreSubmitted: boolean = false;
 
   submitScoreForm: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required)
@@ -48,10 +49,12 @@ export class ScoreboardComponent {
 
   @Output() playClicked: EventEmitter<void> = new EventEmitter<void>();
   onClickPlay() {
+    this.scoreSubmitted = false;
     this.playClicked.emit();
   }
 
   onSubmit() {
+    this.scoreSubmitted = true;
     console.log(this.submitScoreForm.value);
     this.httpClient
       .post('https://aim-game-834f0-default-rtdb.europe-west1.firebasedatabase.app/scores.json',
@@ -59,7 +62,10 @@ export class ScoreboardComponent {
           name: this.submitScoreForm.value.name,
           score: this.score
         })
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        console.log(response);
+
+      });
   }
 
 }
